fbd339f8c5900407390096dcf3f5fe5bd80acc71
mishrayash18 <yashbmishra@gmail.com>
2026-07-30 11:04:28 +0530

    solved problem 160, intersection node of two linked list (brute force) on leetcode

---
+++ leetcode/intersectionoftwoLL_160.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+    int data;
+    node* next;
+
+    node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+node* getIntersectionNode(node* headA, node* headB) {
+    node* tempA = headA;
+    node* tempB = headB;
+    while (tempA!=nullptr) {
+        while (tempB!=nullptr){
+            if (tempA == tempB ) {
+                return tempA;
+            }
+            tempB=tempB->next;
+        }
+        tempA=tempA->next;
+        tempB=headB;
+    }
+    return nullptr;
+}
+
+int main() {
+    node* headA = new node(10);
+    headA -> next = new node(20);
+    headA -> next -> next = new node(30);
+
+    node* headB = new node(40);
+    headB -> next = new node(50);
+    headB -> next -> next = headA -> next -> next;
+
+    node* ans = getIntersectionNode(headA, headB);
+    cout << ans->data;
+}
