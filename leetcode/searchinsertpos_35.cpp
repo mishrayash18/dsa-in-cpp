529ea57e10826431ccb4d8bfbbb18cb67cce6f23
mishrayash18 <yashbmishra@gmail.com>
2026-08-08 11:38:46 +0530

    solved problem 35, search insert position on leetcode

---
+++ leetcode/searchinsertpos_35.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int searchInsert(vector<int> nums, int target) {
+    int left = 0;
+    int right = nums.size()-1;
+
+    while (left<=right) {
+        int mid  = left+((right-left)/2);
+
+        if (nums[mid] == target) return mid;
+
+        if (nums[mid] < target) {
+            left = mid+1;
+        }
+
+        else right = mid-1;
+    }
+
+    return left;
+}
+
+int main() {
+    cout << searchInsert({1,3,5,6}, 0);
+}
