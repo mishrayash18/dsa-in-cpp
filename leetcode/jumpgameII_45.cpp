adfd148cf21a5dc0466c25ca1fb8df0540a9b496
mishrayash18 <yashbmishra@gmail.com>
2026-08-10 23:31:27 +0530

    solved problem 45, jumpgameII on leetcode

---
+++ leetcode/jumpgameII_45.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int jump(vector<int> nums) {
+    int jumps = 0;
+    int currentEnd = 0;
+    int farthest = 0;
+    for (int i=0; i<nums.size()-1; i++) {
+        farthest = max(farthest, i+nums[i]);
+
+        if (i == currentEnd) {
+            jumps++;
+            currentEnd = farthest;
+        }
+    }
+    return jumps;
+}
+int main() {
+    cout << jump({1,3,4,1,1,2});
+
+}
