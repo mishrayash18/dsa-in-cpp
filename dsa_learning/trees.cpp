9e96f71037370246641147404469beb9d39bd766
mishrayash18 <yashbmishra@gmail.com>
2026-08-26 17:20:12 +0530

    implmented dfs, preorder, inorder and postorder on a binary tree

--- dsa_learning/trees.cpp
+++ dsa_learning/trees.cpp
@@ -67,3 +67,66 @@
 //     return 0;
 
 // }
+
+
+
+
+// DFS TRAVERSAL
+// PREORDER TRAVERSAL
+#include <iostream>
+using namespace std;
+
+
+struct node {
+    int data;
+    node* left;
+    node* right;
+
+    node(int x) {
+        data=x;
+        left=nullptr;
+        right=nullptr;
+    }
+};
+
+
+void preorder(node* root) {
+    if (root == nullptr) return;
+
+    cout << root->data << endl;
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(node* root) {
+    if (root == nullptr) return;
+
+    inorder(root->left);
+    cout << root->data << endl;
+    inorder(root->right);
+}
+
+void postorder(node* root) {
+    if (root == nullptr) return;
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << endl;
+}
+
+int main() {
+    node* root = new node(10);
+
+    root->left = new node(5);
+    root->right = new node(20);
+
+    root->left->left = new node(3);
+    root->left->right = new node(7);
+
+    root->left->right->right = new node(8);
+
+    preorder(root);
+    inorder(root);
+    postorder(root);
+
+}
