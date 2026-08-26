1befced61da39c5f224fe3d89c22325d3e6ef9e9
mishrayash18 <yashbmishra@gmail.com>
2026-08-26 17:07:49 +0530

    solved basic of binary trees, finding the height of a tree

--- dsa_learning/trees.cpp
+++ dsa_learning/trees.cpp
@@ -1,23 +1,69 @@
-// creating a basic tree ds
-#include <iostream>
-using namespace std;
+// CREATING A BASIC TREE DS
+// #include <iostream>
+// using namespace std;
 
-struct node {
-    int data;
-    node* left;
-    node* right;
+// struct node {
+//     int data;
+//     node* left;
+//     node* right;
 
-    node(int x) {
-        data=x;
-        left=nullptr;
-        right=nullptr;
-    }
-};
+//     node(int x) {
+//         data=x;
+//         left=nullptr;
+//         right=nullptr;
+//     }
+// };
 
-int main() {
-    node* root = new node(10);
-    root->left = new node(20);
-    root->right = new node(30);
+// int main() {
+//     node* root = new node(10);
+//     root->left = new node(20);
+//     root->right = new node(30);
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+
+// FINDING THE HEIGHT  OF A TREE USING BACKTRACKING
+// #include <iostream>
+// using namespace std;
+
+// struct node {
+//     int data;
+//     node* left;
+//     node* right;
+
+//     node(int x) {
+//         data=x;
+//         left=nullptr;
+//         right=nullptr;
+//     }
+// };
+
+// int height(node* root) {
+//     if (root == nullptr) return 0;
+
+//     int leftheight = height(root->left);
+//     int rightheight = height(root->right);
+
+//     int myheight = 1+max(leftheight,rightheight);
+
+//     return myheight;
+// }
+
+
+// int main() {
+//     node* root = new node(10);
+
+//     root->left = new node(5);
+//     root->right = new node(20);
+
+//     root->left->left = new node(3);
+//     root->left->right = new node(7);
+
+//     root->left->right->right = new node(8);
+
+//     cout<<height(root);
+//     return 0;
+
+// }
