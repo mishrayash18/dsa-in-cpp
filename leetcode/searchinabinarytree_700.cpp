3bfdf1435adfa95abd9767aaf61b20aa37f78348
mishrayash18 <yashbmishra@gmail.com>
2026-09-14 22:40:31 +0530

    solved problem 100, search in a binary search tree on leetcode

---
+++ leetcode/searchinabinarytree_700.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+    node* left;
+    node* right;
+    int data;
+
+    node (int x) {
+        left=right=nullptr;
+        data=x;
+    }
+};
+
+node* searchBST(node* root, int val) {
+    if (root == nullptr) return nullptr;
+
+    else if (val == root->data) return root;
+    
+    else if (val >= root->data) {
+        node* leftnode = searchBST(root->right, val);
+    }
+    
+    else if (val <= root->data) {
+        node* rightnode = searchBST(root->left, val);
+    }
+
+}
+
+int main() {
+    node* root = new node(10);
+
+    root->left = new node(5);
+    root->right = new node(20);
+
+    root->left->left = new node(3);
+    root->left->right = new node(7);
+
+    root->left->right->right = new node(8);
+
+    node* 
+
+    
+
+    return 0;
+
+}
